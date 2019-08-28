60c117334e5698935ef3c5c834677ef9a27b21c1
MehmetSiar <siarsoylemez@gmail.com>
2019-08-28 14:10:46 +0300

    Traffic Api

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SmartiksProje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartiksProje.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+         
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Mapping map)
+        {
+            Mapping mappAdresi = new Mapping();
+            mappAdresi.id = map.id;
+            mappAdresi.kazaAciklama = map.kazaAciklama;
+            mappAdresi.kazaTarihi = map.kazaTarihi;
+            mappAdresi.kazasaati = map.kazasaati;
+
+            return View(mappAdresi);
+        }
+    }
+}
---
+++ Models/Mapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartiksProje.Models
+{
+    public class Mapping
+    {
+        public int id { get; set; }
+        public DateTime kazaTarihi { get; set; }
+        public DateTime kazasaati { get; set; }
+        public string kazaAciklama { get; set; }
+        public decimal x { get; set; }
+        public decimal y { get; set; }
+
+
+    }
+}
